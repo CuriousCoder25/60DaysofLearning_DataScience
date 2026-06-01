2b2adf95be90a426d1579a2cf3cf3ccf9fce3d3b
gaurav <gauravkathayat12@gmail.com>
2026-06-01 22:37:42 +0545

    Initial Commit -#60Daysoflearning

---
+++ day1.py
@@ -0,0 +1,18 @@
+
+user_name = "Gaurav"          
+current_day = 1               
+progress_percentage = 1.5   
+
+print("- Day 1 Python Execution -")
+print("Learner:", user_name)
+print("Challenge Day:", current_day)
+print("Progress:", progress_percentage, "%")
+
+# 3. Verifying Dynamic Typing
+# Python automatically detects the data type. Let's prove it:
+print("\n- Under the Hood (Data Types) -")
+print("Type of user_name:", type(user_name))
+print("Type of current_day:", type(current_day))
+print("Type of progress_percentage:", type(progress_percentage))
+
+# Goal: Understand how Python stores data in memory dynamically.
